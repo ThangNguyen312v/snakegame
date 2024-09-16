cc9ef2a8a0a89bf268fd8bcf983b2a4a08535ec6
ThangNguyen312v <tatthang112113@gmail.com>
2024-09-16 07:01:54 +0700

    baitap

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+namespace snakeGame{
+class Program{
+    static void Main(string[] Args){
+        Thread _game = new Thread   (snakeCTR.ListenKey);
+        _game.Start();
+        do{
+            
+            Console.Clear();snakeCTR.Drawboard();
+            snakeCTR.setUpBoard();
+            snakeCTR.MoveHead();
+            snakeCTR.EatFood();
+            snakeCTR.PopupFood();
+            snakeCTR.SpawnBody();
+            snakeCTR.ShowPoint();
+            Task.Delay(snakeCTR.speed).Wait();
+
+        }while(true);
+    }
+}
+}
